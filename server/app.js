8c8d31566045fd6fc312a64d0ccec282e142e26f
T-Dourgarian <thomasdourgarian@gmail.com>
2020-08-28 19:56:10 -0500

    5 minute request to keep heroku awake

--- server/app.js
+++ server/app.js
@@ -36,6 +36,9 @@ const userRouter = require('./routes/user.router');
 app.use('/api/account', accountRouter);
 app.use('/api/indicator', indicatorRouter);
 app.use('/api/user', userRouter);
+app.get('/public',(req,res) => {
+	res.sendStatus(200);
+})
 
 // Serve static files
 app.use(express.static('build'));
@@ -46,9 +49,14 @@ if (process.env.NODE_ENV === 'production') {
 	app.get(/.*/, (req,res) => {
 		res.sendFile(__dirname + '/public/index.html')
 	});
+
+
+	setInterval(function() {
+		axios.get("https://td-binance.herokuapp.com/public");
+	}, 300000); // every 5 minutes (300000)
+
 }
 
-// getRSIData();
 
 // App Set //
 const PORT = process.env.PORT || 3000;
